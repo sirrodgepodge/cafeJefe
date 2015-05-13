6e9b3b0a4489bd9e3d02d68d325eb68d9dff397a
Rodge Rodgena Na <rodgena@AdminisatorsMBP.home>
2015-05-13 13:44:56 -0400

    test

--- bin/www.js
+++ bin/www.js
@@ -1,16 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/env node;
 
-/**
- * Module dependencies.
- */
+//Module dependencies
 
 var app = require('../app');
 var debug = require('debug')('cafeJefe:server');
 var http = require('http');
 
-/**
- * Get port from environment and store in Express.
- */
+//Get port from environment and store in Express.
 
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
