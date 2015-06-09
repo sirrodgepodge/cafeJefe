5c6314b35b9a1655ed4876f2d1e8992aee6bddd2
sirrodgepodge <rwcbeaman@gmail.com>
2015-06-09 21:24:37 +0000

    stylin

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,46 +12,46 @@ var prices={
 };
 
 var main = function() {
-	var purchTypeVal= $('.active-pill').attr('id');
-	var inc= 0;
-	$price.each(function(){
-		$(this).html('$'+prices[purchTypeVal][inc]);
-		inc++;
-	});
-
+    var purchTypeVal= $('.active-pill').attr('id');
+    var inc= 0;
+    $price.each(function(){
+	$(this).html('$'+prices[purchTypeVal][inc]);
+	inc++;
+    });
+    
     $purchase_toggle.click(function(){
 		if(!$(this).hasClass('active-pill')){
-			$purchase_toggle.each(function() {
-				$(this).toggleClass('active-pill');
-			});
-			purchTypeVal = $(this).attr('id');
-			$('.button-text').html(purchTypeVal.slice(0,1).toUpperCase()+purchTypeVal.slice(1));
-			inc = 0;
-			$price.each(function(){
-				$(this).html('$'+prices[purchTypeVal][inc]);
-				inc++;
-			});
-
-			$buy_btn.each(function(){
-				var currVal = $(this).attr('href').slice(0, $(this).attr('href').lastIndexOf("-")+1);
-				$(this).attr('href', currVal + purchTypeVal);
-			});
+		    $purchase_toggle.each(function() {
+			$(this).toggleClass('active-pill');
+		    });
+		    purchTypeVal = $(this).attr('id');
+		    $('.button-text').html(purchTypeVal.slice(0,1).toUpperCase()+purchTypeVal.slice(1));
+		    inc = 0;
+		    $price.each(function(){
+			$(this).html('$'+prices[purchTypeVal][inc]);
+			inc++;
+		    });
+		    
+		    $buy_btn.each(function(){
+			var currVal = $(this).attr('href').slice(0, $(this).attr('href').lastIndexOf("-")+1);
+			$(this).attr('href', currVal + purchTypeVal);
+		    });
 		}
 	});
-
-	$label.click(function(){
-		if(!$(this).hasClass('active-text')){
-			$('.active-text').toggleClass('active-text');
-			$(this).toggleClass('active-text');
-		}
-	});
-
-	$button_fill.hover(function () {
-		$(this).children(".button-inside").addClass('full');
-	}, function() {
-		$(this).children(".button-inside").removeClass('full');
-	});
-
+    
+    $label.click(function(){
+	if(!$(this).hasClass('active-text')){
+	    $('.active-text').toggleClass('active-text');
+	    $(this).toggleClass('active-text');
+	}
+    });
+    
+    $button_fill.hover(function () {
+	$(this).children(".button-inside").addClass('full');
+    }, function() {
+	$(this).children(".button-inside").removeClass('full');
+    });
+    
     var addresses = ["6147 Lakeside Drive, Reno, NV 89502","300 W Rosemary Lane, falls church, va 22046"];
     
     var toLatLng = function(addressArr) {
@@ -65,27 +65,27 @@ var main = function() {
 	}
 	return results;
     }
-
+    
     var addrLatLng = toLatLng(addresses);
     console.log(addrLatLng);
     var map = $("#map").gmap3({
-		marker: {
-		    address: "300 W Rosemary Lane, falls church, va 22046",
-		    options:{
-			icon: "images/Marker_Mask.png"
-		    }
-		},
-		map:{
-		    options:{
-			styles: [{
-			    stylers: [ { "saturation":-100 }, { "lightness": 0 }, { "gamma": 0.5 }]
-			}],
-			zoom: 15,
-			scrollwheel:false,
-			draggable: true,
-			center: new google.maps.LatLng(addrLatLng[0][0],addrLatLng[0][1])
-		    }
-		}
+	marker: {
+	    address: "300 W Rosemary Lane, falls church, va 22046",
+	    options:{
+		icon: "images/Marker_Mask.png"
+	    }
+	},
+	map:{
+	    options:{
+		styles: [{
+		    stylers: [ { "saturation":-100 }, { "lightness": 0 }, { "gamma": 0.5 }]
+		}],
+		zoom: 15,
+		scrollwheel:false,
+		draggable: true,
+		center: new google.maps.LatLng(addrLatLng[0][0],addrLatLng[0][1])
+	    }
+	}
     });
     console.dir(map);
 };
