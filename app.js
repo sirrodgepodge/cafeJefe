ed39a0027314254afc9952e9fee608838b18de3d
sirrodgepodge <rwcbeaman@gmail.com>
2015-06-10 14:51:54 +0000

    bowerrrc

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/bower_components',  express.static(path.join(__dirname,'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
