f3be9f3a688bf5684db3743aea50bde5582ec915
sirrodgepodge <rwcbeaman@gmail.com>
2015-06-10 18:11:03 +0000

    stylin

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -61,7 +61,7 @@ var main = function() {
     });
 
     $backToTop.click(function(){
-	$('body').animate({scrollTop:0},1000);
+	$('body').animate({scrollTop:0}, $(window).scrollTop());
     });
 };
 
