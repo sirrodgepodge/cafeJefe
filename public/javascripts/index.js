915c9b93329bb4006d72666bbbe9a992e9294645
Roger Beaman <rwcbeaman@gmail.com>
2015-06-10 00:10:07 -0400

    stylin

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -21,28 +21,28 @@ var main = function() {
     
     $purchase_toggle.click(function(){
 		if(!$(this).hasClass('active-pill')){
-		    $purchase_toggle.each(function() {
-			$(this).toggleClass('active-pill');
-		    });
-		    purchTypeVal = $(this).attr('id');
-		    $('.button-text').html(purchTypeVal.slice(0,1).toUpperCase()+purchTypeVal.slice(1));
-		    inc = 0;
-		    $price.each(function(){
-			$(this).html('$'+prices[purchTypeVal][inc]);
-			inc++;
-		    });
-		    
-		    $buy_btn.each(function(){
+			$purchase_toggle.each(function() {
+				$(this).toggleClass('active-pill');
+			});
+			purchTypeVal = $(this).attr('id');
+			$('.button-text').html(purchTypeVal.slice(0,1).toUpperCase()+purchTypeVal.slice(1));
+			inc = 0;
+			$price.each(function(){
+				$(this).html('$'+prices[purchTypeVal][inc]);
+				inc++;
+			});
+
+			$buy_btn.each(function(){
 			var currVal = $(this).attr('href').slice(0, $(this).attr('href').lastIndexOf("-")+1);
 			$(this).attr('href', currVal + purchTypeVal);
-		    });
+		});
 		}
 	});
     
     $label.click(function(){
 	if(!$(this).hasClass('active-text')){
-	    $('.active-text').toggleClass('active-text');
-	    $(this).toggleClass('active-text');
+		$('.active-text').toggleClass('active-text');
+		$(this).toggleClass('active-text');
 	}
     });
     
@@ -55,20 +55,24 @@ var main = function() {
     var addresses = ["6147 Lakeside Drive, Reno, NV 89502","300 W Rosemary Lane, falls church, va 22046"];
     
     var toLatLng = function(addressArr, cb) {
-	var addressArr= typeof addressArr==='object'&&addressArr||[addressArr];
-	var results = [];
-	for(var i = 0; i<addressArr.length; i++){
-	    $.getJSON('http://maps.googleapis.com/api/geocode/json?address='+addressArr[i]+'&sensor=false', null, function(data) {
-		var addrObj = data.results[0].geometry.location;
-		results.push([addrObj.lat, addrObj.lng]);
-	    })
-	}
-	cb(null, results);
-    }
-    
+		addressArr= typeof addressArr==='object'&&addressArr||[addressArr];
+		var results = [];
+		for(var i = 0; i<addressArr.length; i++){
+			$.getJSON('http://maps.googleapis.com/api/geocode/json?address='+addressArr[i]+'&sensor=false&key=AIzaSyCy6aNLouwxXosCK0PdQ5P2vm578iUqlM4', null, function(data) {
+				var addrObj = data.results[0].geometry.location;
+				results.push([addrObj.lat, addrObj.lng]);
+			});
+		}
+		while(true){
+			if(results.length===addressArr.length) return cb(null, results);
+		}
+    };
+
+    console.log(addresses);
+
     toLatLng(addresses, function(err, data){
-	console.log(data);
-	var map = $("#map").gmap3({
+		console.log(data);
+		var map = $("#map").gmap3({
             marker: {
                 address: "300 W Rosemary Lane, falls church, va 22046",
                 options:{
