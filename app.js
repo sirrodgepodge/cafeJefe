2fa5b8c7505956a2ba8b0ddd5650fdd664c5884c
sirrodgepodge <rwcbeaman@gmail.com>
2015-06-16 15:35:54 +0000

    scss

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'assets', 'views'));
 
 // loading middlewares
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
