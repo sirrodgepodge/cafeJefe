fa39999e58ca651c693c1bb1a9f8165d7f21d1ee
sirrodgepodge <rwcbeaman@gmail.com>
2015-06-25 15:55:36 +0000

    style

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sass = require('node-sass-middleware');
+var compression = require('compression');
 
 // get routes from 'routes' folder
 var routes = require('./routes/index');
@@ -17,6 +18,7 @@ app.set('view engine', 'jade');
 app.set('views', path.join(__dirname,'assets', 'views'));
 
 // loading middlewares
+app.use(compression());
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
