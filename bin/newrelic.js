61382e66afd4491301b332317f4c99b3ba8dd17d
sirrodgepodge <rwcbeaman@gmail.com>
2015-07-01 20:20:16 +0000

    added dyno...

---
+++ bin/newrelic.js
@@ -0,0 +1,13 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name: ['cafejefe'],
+  license_key: '23574295953b60665bfb3a1a94e187f4d7046e6a',
+  logging: {
+    level: 'info'
+  }
+}
