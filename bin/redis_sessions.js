0440966c8f7ba953906cf5ec18b64d897fe3285c
sirrodgepodge <rwcbeaman@gmail.com>
2015-08-04 21:02:27 +0000

    working or no...

---
+++ bin/redis_sessions.js
@@ -0,0 +1,23 @@
+var router = require('express').Router(),
+    redis = require('redis'),
+    session = require('express-session');
+
+var redisStore = require('connect-redis')(session),
+    redisClient = redis.createClient(); //CREATE REDIS CLIENT
+
+router.use(session({
+    secret: 'mysecretcode',
+    store: new redisStore({
+        host: process.env.NODE_ENV==='production'?'www.cafejefe.com':'localhost', //states the host to listen on
+        port: 6379, //listen for redis db on this port
+        client: redisClient,
+        ttl: 900 //time out after 15 minutes
+    }),
+    saveUninitialized: false, // don't create session until something stored,
+    resave: false, // don't save session if unmodified
+    cookie: {secure: process.env.NODE_ENV==='production'? true: false,
+	     maxAge: 162000000 //cookie times out after 45 mins, number is in milliseconds
+	    }
+}));
+
+module.exports = router;
